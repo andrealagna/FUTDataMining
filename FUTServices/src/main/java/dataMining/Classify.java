6656fd5a3376ab9f394ea6990a50003a1b2ee295
Andrea Lagna <andrealagna7@gmail.com>
2021-02-20 14:11:10 +0100

    data mining

--- FUTServices/src/main/java/dataMining/Classify.java
+++ FUTServices/src/main/java/dataMining/Classify.java
@@ -1,67 +1,73 @@
 package dataMining;
 
 import bean.Comment;
-import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.bayes.NaiveBayesMultinomialText;
 import weka.core.*;
 import weka.core.SerializationHelper;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Classify {
 
-    public ArrayList<Comment> labelComments(ArrayList<Comment> c) throws Exception{
+    public ArrayList<Comment> cleanComments(ArrayList<Comment> comments) throws Exception{
+        String cleanedComment;
+        for(int i = 0; i < comments.size(); i++) {
+            cleanedComment = comments.get(i).getText();
+            cleanedComment.replaceAll("\\s+", " ");
+            cleanedComment.replaceAll("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"," ");
+        }
+        return comments;
+    }
 
-        ArrayList<String> classLabels;
-        classLabels = new ArrayList<String>();
-        classLabels.add("negative");
-        classLabels.add("neutral");
-        classLabels.add("positive");
 
-        //Attributes
+    public ArrayList<Comment> labelComments(ArrayList<Comment> comments) throws Exception{
+
+        ArrayList<String> classes;
+        classes = new ArrayList<String>();
+        classes.add("negative");
+        classes.add("neutral");
+        classes.add("positive");
+
         Attribute clas;
         Attribute text;
         text = new Attribute("text", true);
-        clas = new Attribute("class", classLabels);
+        clas = new Attribute("class", classes);
 
-        //List of attributes
         ArrayList<Attribute> attributes;
         attributes = new ArrayList<Attribute>();
         attributes.add(text);
         attributes.add(clas);
 
-        //Dataset
         Instances dataset;
-        Instance inst;
-        dataset = new Instances("comments", attributes, c.size());
+        Instance instance;
+        dataset = new Instances("comments", attributes, comments.size());
         dataset.setClassIndex(dataset.numAttributes()-1);
 
-        //Creation of the instances of the dataset and set the class as missing (unlabeled)
-        for(int i = 0; i < c.size(); i++) {
+        for(int i = 0; i < comments.size(); i++) {
             double[] value = new double[dataset.numAttributes()];
-            value[0] = dataset.attribute(0).addStringValue(c.get(i).getText());
-            inst = new DenseInstance(1.0, value);
-            dataset.add(inst);
+            value[0] = dataset.attribute(0).addStringValue(comments.get(i).getText());
+            instance = new DenseInstance(1.0, value);
+            dataset.add(instance);
             dataset.instance(i).setClassMissing();
         }
 
-        FilteredClassifier classifier;
-        classifier = (FilteredClassifier) SerializationHelper.read("C:\\Users\\Andrea Lagna\\IdeaProjects\\FUTDM\\FUTServices\\src\\main\\resources\\SMO.model");
+        NaiveBayesMultinomialText classifier;
+        classifier = (NaiveBayesMultinomialText) SerializationHelper.read(
+                "../FUTServices/src/main/resources/NaiveBayesMultinomialText_sliding_6.model");
 
-        for(int i = 0; i < c.size(); i++) {
+        for(int i = 0; i < comments.size(); i++) {
             double predicted;
             dataset.instance(i).setClassValue(classifier.classifyInstance(dataset.instance(i)));
             predicted = dataset.instance(i).classValue();
-            if(predicted == 0) { //Index of the class label array -> 0: negative. 1: neutral. 2:positive
-                c.get(i).setSentiment("negative");
+            if(predicted == 0) { //0: negative. 1: neutral. 2:positive
+                comments.get(i).setSentiment("negative");
             } else if (predicted == 2) {
-                c.get(i).setSentiment("positive");
+                comments.get(i).setSentiment("positive");
             }else{
-                c.get(i).setSentiment("neutral");
+                comments.get(i).setSentiment("neutral");
             }
         }
-        return c;
+        return comments;
     }
 }
+
+
